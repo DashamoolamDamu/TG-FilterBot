e990cc85c6a164ee6ede5043cb6fefa8de0ebdd0
botxeditor <107136639+botxeditor@users.noreply.github.com>
2022-06-14 10:51:42 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -47,7 +47,7 @@ async def start(client, message):
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         await message.reply_chat_action("typing")
-        m=await message.reply_sticker("CAACAgUAAxkBAAPLYhmLeQjtiqPZJEHOFh4KFOjbWzcAAvgDAAKfRIlXnd2oxpsLJeYeBA")
+        m=await message.reply_sticker("CAACAgIAAxkBAAECmmFiqBqsbl4rDmZ5x8So_SNJDpnnCwACVBYAAtB7QUn8uVjZ80ZWKB4E")
         await asyncio.sleep(1)
         await m.delete()
         await message.reply_chat_action("typing")
