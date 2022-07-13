42d09d3d37622184801473cae359f28ab9562d1d
ᴍᴇ!!! <107136639+botxeditor@users.noreply.github.com>
2022-07-13 21:38:06 +0530

    Update welcome.py

--- plugins/welcome.py
+++ plugins/welcome.py
@@ -7,7 +7,7 @@ from pyrogram.types import Message, User
 @Client.on_message(filters.new_chat_members)
 async def welcome(bot,message):
 	chatid= message.chat.id
-	await bot.send_message(text=f"Welcome {message.from_user.mention} to {message.chat.username} ,  Happy to have here",chat_id=chatid)
+	await bot.send_message(text=f"Hyy {message.from_user.mention} Welcome to {message.chat.username} /n 📍 No Promo, No Porn, No Other Abuses /n 📍 Ask Your Movies With Correct Spelling /n 📍 Spammers Stay Away /n 📍 Feel Free To Report Any Errors To Admins using @admin /n/n 🔰 Share & Support Us 🔰",chat_id=chatid)
 	
 @Client.on_message(filters.left_chat_member)
 async def goodbye(bot,message):
